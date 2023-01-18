3139b39c74a5e786e6bf15d95bd8a6fe5c9c3f45
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-01-17 21:21:46 -0300

    feat: Adicionando novos atributos a entidade Neo

--- lib/entity/neo.dart
+++ lib/entity/neo.dart
@@ -1,11 +1,23 @@
 class Neo {
-  Neo(this._name,
-      this._absoluteMagnitudeH,
-      this._isPotentiallyHazardousAsteroid,
-      this._isSentryObject);
+  Neo(
+    this._name,
+    this._absoluteMagnitudeH,
+    this._minEstimatedDiameter,
+    this._maxEstimatedDiameter,
+    this._closeApproachDate,
+    this._kilometersPerSecond,
+    this._missDistance,
+    this._isPotentiallyHazardousAsteroid,
+    this._isSentryObject,
+  );
 
   final String _name;
   final double _absoluteMagnitudeH;
+  final double _minEstimatedDiameter;
+  final double _maxEstimatedDiameter;
+  final String _closeApproachDate;
+  final String _kilometersPerSecond;
+  final String _missDistance;
   final bool _isPotentiallyHazardousAsteroid;
   final bool _isSentryObject;
 
@@ -13,7 +25,8 @@ class Neo {
 
   double get returnAbsoluteMagnitudeH => this._absoluteMagnitudeH;
 
-  bool get returnIsPotentiallyHazardousAsteroid => this._isPotentiallyHazardousAsteroid;
+  bool get returnIsPotentiallyHazardousAsteroid =>
+      this._isPotentiallyHazardousAsteroid;
 
   bool get returnIsSentryObject => this._isSentryObject;
 }
--- lib/requisition/requisition.dart
+++ lib/requisition/requisition.dart
@@ -26,6 +26,12 @@ class Requisiton {
         Neo neo = Neo(
           neoMap['name'],
           neoMap['absolute_magnitude_h'],
+          neoMap['estimated_diameter']['kilometers']['estimated_diameter_min'],
+          neoMap['estimated_diameter']['kilometers']['estimated_diameter_max'],
+          neoMap['close_approach_data'][0]['close_approach_date_full'],
+          neoMap['close_approach_data'][0]['relative_velocity']
+              ['kilometers_per_second'],
+          neoMap['close_approach_data'][0]['miss_distance']['kilometers'],
           neoMap['is_potentially_hazardous_asteroid'],
           neoMap['is_sentry_object'],
         );
